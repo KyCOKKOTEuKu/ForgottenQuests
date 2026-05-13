2dd0a82f4d62ea8734bd3bd70d91b5017cc8e312
KyCOKKOTEuKu <137386694+KyCOKKOTEuKu@users.noreply.github.com>
2026-05-13 21:18:56 +0300

    Delete QuestTaskType.cs

--- QuestTaskType.cs
+++
@@ -1,9 +0,0 @@
-namespace ForgottenQuests;
-
-public class QuestProgress
-{
-    public string QuestId { get; set; } = "";
-    public int CurrentAmount { get; set; }
-    public bool Completed { get; set; }
-    public long LastCompletedUnixTime { get; set; }
-}
